8b84c955718e442a0b1aa1a4d97c53cc15a795ec
Moolmancell <richmondclaudebodot@gmail.com>
2024-08-15 20:49:32 +0800

    Finish basic todo list running from console

--- src/index.js
+++ src/index.js
@@ -1,24 +1,79 @@
+const LocalStorageAdaptor = (function() {
+
+    function getKey(key) {
+        return JSON.parse(localStorage.getItem(key));
+    }
+
+    function setKey(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    function removeKey(key) {
+        localStorage.removeItem(key);
+    }
+
+    return {
+        getKey,
+        setKey,
+        removeKey
+    }
+})();
+
 function addProject(name, description) {
-    if (localStorage.getItem(name)) {
-        console.log("Please change Project Name");
+    if (LocalStorageAdaptor.getKey(name)) {
+        console.log("Project already exists. Please choose a different name.");
         return;
-    } 
-    localStorage.setItem(name, JSON.stringify({name, description, todo: []}));
-    //return {name, description, todo: };
+    }
+    LocalStorageAdaptor.setKey(name, { name, description, todo: [] });
 }
 
-function addToDo(title, description, duedate, priority, proj = "default") {
-    let newList = JSON.parse(localStorage.getItem(proj));
-    newList["todo"].push({
-        title, 
-        description, 
-        duedate, 
-        priority
-    });
-    localStorage.setItem(proj, JSON.stringify(newList))
-    //return {title, description, duedate, priority};
+function addToDo(title, description, duedate, priority, proj = "_default") {
+    let project = LocalStorageAdaptor.getKey(proj);
+    if (!project) {
+        console.log("Project does not exist.");
+        return;
+    }
+    project.todo.push({ title, description, duedate, priority });
+    LocalStorageAdaptor.setKey(proj, project);
 }
 
 function checkProject(name) {
-    console.log(JSON.parse(localStorage.getItem(name)))
+    const project = LocalStorageAdaptor.getKey(name);
+    if (project) {
+        console.log(project);
+    } else {
+        console.log("Project does not exist.");
+    }
 }
+
+function removeTodo(projectName, todoIndex) {
+    let project = LocalStorageAdaptor.getKey(projectName);
+    if (!project) {
+        console.log("Project does not exist");
+        return;
+    }
+    if (todoIndex < 0 || todoIndex >= project.todo.length) {
+        console.log("Invalid to-do index");
+        return;
+    }
+    project.todo.splice(todoIndex, 1);
+    LocalStorageAdaptor.setKey(projectName, project);
+}
+
+function removeProject(projectName) {
+    if (!LocalStorageAdaptor.getKey(projectName)) {
+        console.log("Project does not exist");
+        return;
+    }
+    LocalStorageAdaptor.removeKey(projectName);
+}
+
+const createDefault = (function() {
+    if (LocalStorageAdaptor.getKey("_default")) {
+        console.log("_default already exists in local storage");
+        return;
+    }
+    LocalStorageAdaptor.setKey("_default", { name: "_default", description: "", todo: [] });
+})();
+
+removeTodo("_default", 1)
