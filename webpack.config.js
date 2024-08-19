92598d5f596520dd77be21508e62b1bf222204b1
Moolmancell <richmondclaudebodot@gmail.com>
2024-08-19 22:05:41 +0800

    Add Bootstrap into dependecies

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
+
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
   mode: "development",
   entry: {
-    index: "./src/index.js",
-    ui: ".src/ui.js",
-    logic: ".src/logic.js"
+    index: "./src/js/index.js",
   },
   devtool: 'inline-source-map',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
@@ -22,9 +27,34 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.scss$/i,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer
+                ]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ],
       },
+      
     ],
   },
 };
