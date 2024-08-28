54b140d2e28d7f9b6f9feb36fc71b11fcf6c41bf
Moolmancell <richmondclaudebodot@gmail.com>
2024-08-28 15:34:01 +0800

    Add addProject JS file and modal

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import * as bootstrap from 'bootstrap'
 import "./ui"
 import "./logic"
 import "./addTask"
+import "./addProject"
 
 import { LocalStorageAdaptor } from "./logic";
 import { generateProjectsSelection } from './ui';
