d263ca0faf9d45bbe34895c80e3c67eae3ff737e
Moolmancell <richmondclaudebodot@gmail.com>
2024-08-30 15:06:53 +0800

    Clean editTodo.js code

--- src/js/editTodo.js
+++ src/js/editTodo.js
@@ -1,23 +1,59 @@
+import * as bootstrap from 'bootstrap';
 import { formatDate } from "./date";
+import { currentProject } from "./ui";
+import { LocalStorageAdaptor } from "./logic";
+import { generateTasks } from "./ui";
+import { removeTodo, addToDo } from './logic';
+
+const editModal = document.getElementById("editTaskModal");
+const editTaskModal = new bootstrap.Modal(editModal);
+const saveEditButton = document.getElementById("save-edit-button-modal");
+
+function setFormValues(item) {
+    document.getElementById("edit-task-title").value = item.title;
+    document.getElementById("edit-task-description").value = item.description;
+    document.getElementById("edit-task-date").value = formatDate(item.duedate);
+    document.getElementById("edit-task-priority").value = item.priority;
+}
+
+function validateForm() {
+    const title = document.getElementById("edit-task-title").value;
+    const description = document.getElementById("edit-task-description").value;
+    const date = document.getElementById("edit-task-date").value;
+    const priority = document.getElementById("edit-task-priority").value;
+    return title && description && date && priority;
+}
+
+function updateTask(project, title) {
+    const newTitle = document.getElementById("edit-task-title").value;
+    const newDescription = document.getElementById("edit-task-description").value;
+    const newDate = document.getElementById("edit-task-date").value;
+    const newPriority = +document.getElementById("edit-task-priority").value;
+
+    removeTodo(project, title);
+    addToDo(newTitle, newDescription, newDate, newPriority, project);
+    generateTasks(LocalStorageAdaptor.getKey(project));
+}
 
 export function editTodo(project, title) {
-    const _title = document.getElementById("edit-task-title");
-    const _description = document.getElementById("edit-task-description");
-    const _date = document.getElementById("edit-task-date");
-    const _priority = document.getElementById("edit-task-priority");
-
-    // Find the todo item by title
     const item = project.todo.find(todoItem => todoItem.title === title);
 
     if (item) {
-        // Set the values of the input fields
-        _title.value = item.title;
-        _description.value = item.description;
-        
-        // Format the date to YYYY-MM-DD
-        const formattedDate = formatDate(item.duedate);
-        _date.value = formattedDate;
-        
-        _priority.value = item.priority;
+        setFormValues(item);
     }
+
+    const handleSaveEdit = () => {
+        if (validateForm()) {
+            updateTask(currentProject, title);
+            editTaskModal.hide();
+        } else {
+            alert("Fill all the required fields");
+        }
+    };
+
+    saveEditButton.addEventListener("click", handleSaveEdit);
+
+    editModal.addEventListener("hide.bs.modal", () => {
+        saveEditButton.removeEventListener("click", handleSaveEdit);
+    });
 }
